0ea309fa0d1f1fee36c17d0aba7b89288d27dd60
Abhilash_Tirumala <abhilash.t@way2online.co.in>
2018-08-22 11:27:25 +0530

    Initial Commit

---
+++ app/src/main/java/sun/way2online/lazypay/demoprojectopendata/MainActivity.java
@@ -0,0 +1,13 @@
+package sun.way2online.lazypay.demoprojectopendata;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
